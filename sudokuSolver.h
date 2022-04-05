a990c2eb7a759151f23fe48f389c9f1228c29a10
Blue1818 <iwadale@gmail.com>
2022-04-05 13:30:52 -0500

    more skillies

--- sudokuSolver.h
+++ sudokuSolver.h
@@ -8,24 +8,45 @@
 #define SUDOKUSOLVER_H
 
 #include <cstdint>  ///standard integer representations
+#include <string>
+
+using std::string;
 
 
 class sudokuSolver
 {
-    public:
+    private:
         uint8_t board[9][9]; //board state of the sudoku board
                              //1 - 9
                              //0 = blank
                              
 
-    private:
+    public:
         //default constructor
         sudokuSolver();
 
+        //construct from file
+        sudokuSolver(string fileName);
 
 
-        
+        //getter
+        uint8_t getCell(uint8_t x, uint8_t y);
 
+        //setter
+        uint8_t setCell(uint8_t x, uint8_t y, uint8_t n);
+
+
+        //check if value is legal => 0 <= val > 9
+        bool isLegal(uint8_t val);
+
+        //output board as a printimg in appropiate format
+        void printer();
+
+        //check if the value at the given coordinates is possible
+        bool possible(uint8_t x, uint8_t y, uint8_t n);
+
+        //function to solve the puzzle from the current boardstate
+        void solver();
 
 };
    
