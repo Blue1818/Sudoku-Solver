1827b6531ec6751f0f879b273f1d2721ed55ff0a
Blue1818 <iwadale@gmail.com>
2022-04-05 13:08:22 -0500

    add skellie

---
+++ sudokuSolver.h
@@ -0,0 +1,33 @@
+///***************************************************************************
+///
+///  Dale Westberg
+///
+///
+///***************************************************************************
+#ifndef SUDOKUSOLVER_H
+#define SUDOKUSOLVER_H
+
+#include <cstdint>  ///standard integer representations
+
+
+class sudokuSolver
+{
+    public:
+        uint8_t board[9][9]; //board state of the sudoku board
+                             //1 - 9
+                             //0 = blank
+                             
+
+    private:
+        //default constructor
+        sudokuSolver();
+
+
+
+        
+
+
+};
+   
+
+#endif
