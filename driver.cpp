3e429d46bb6c37678cac182d16ea70df24e500a7
Blue1818 <iwadale@gmail.com>
2022-04-13 11:42:19 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -10,11 +10,11 @@ int main()
     memory mem(0x100);
 
     //memory before file
-    mem.dump();
+    //mem.dump();
     
     //load puzzle
     //if fail then show error
-    if (!mem.load_file("samplePuzzles/ex1.ss"))
+    if (!mem.load_file("samplePuzzles/exOld.ss"))
     {
         std::cerr << "can't load file";
     }
