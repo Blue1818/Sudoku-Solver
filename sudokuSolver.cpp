891f8d34180c65bdc22c681e977683cf461c9057
Blue1818 <iwadale@gmail.com>
2022-04-13 12:07:13 -0500

    Update sudokuSolver.cpp

--- sudokuSolver.cpp
+++ sudokuSolver.cpp
@@ -35,6 +35,7 @@ sudokuSolver::sudokuSolver(string fileName)
 {
     memory mem(0x100);
     uint32_t head = 0x00000000;
+    uint8_t val = 0x00;
 
     //load puzzle
     //if fail then show error
@@ -44,21 +45,101 @@ sudokuSolver::sudokuSolver(string fileName)
         exit(1);
     }
 
+    //null the board
+    nullBoard();
+
     //nested for loop to loop through board
     for(uint8_t y = 0x00; y < 0x09; y++)
     {
         for(uint8_t x = 0x00; x < 0x09; x++)
         {
+            //get byte at address and assign it to val
+            val = mem.get8(head);
 
-            board[x][y] = 0x00;
+            //decode val into 0-9;
+            switch( val )
+            {
+                //1 - 9
+                case 0x31: setCell(x, y , 0x01);
+                break;
+                
+                case 0x32: setCell(x, y , 0x02);
+                break;
+
+                case 0x33: setCell(x, y , 0x03);
+                break;
+
+                case 0x34: setCell(x, y , 0x04);
+                break;
+
+                case 0x35: setCell(x, y , 0x05);
+                break;
+
+                case 0x36: setCell(x, y , 0x06);
+                break;
+
+                case 0x37: setCell(x, y , 0x07);
+                break;
+
+                case 0x38: setCell(x, y , 0x08);
+                break;
+
+                case 0x39: setCell(x, y , 0x09);
+                break;
+
+
+                //its a X, turn it into a 0
+                case 0x58: setCell(x, y , 0x00f9y);
+                break;
+
+                default: 
+                    break;
+            }
+
+            //incrment head
+            head++;
         }
+        //newline, incrment head twice
+        head += 0x00000002;
     }
 
-
+    //check boardstate
+    //if false then print error and exit 1
+    if (!isLegalBoard())
+    {
+        std::cerr << "error: board not legal";
+        exit(1);
+    }
     
 }
 
 
+//set each cell to 0xf5
+void sudokuSolver::nullBoard()
+{
+    for(uint8_t y = 0x00; y < 0x09; y++)
+    {
+        for(uint8_t x = 0x00; x < 0x09; x++)
+        {
+            setCell(x, y, 0xf5);
+        }
+    }
+}
+
+
+//check boardstate
+bool sudokuSolver::isLegalBoard()
+{
+    for(uint8_t y = 0x00; y < 0x09; y++)
+    {
+        for(uint8_t x = 0x00; x < 0x09; x++)
+        {
+            if (isLegal(getCell(x, y)))
+        }
+    }
+
+    return true;
+}
 
 
 //check if value is legal: 0 <= val < 10
