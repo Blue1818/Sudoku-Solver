8fadd8d863a174a856d64f5f3b89185e55f1239d
Blue1818 <iwadale@gmail.com>
2022-04-13 11:08:03 -0500

    Update maker.sh

--- maker.sh
+++ maker.sh
@@ -2,10 +2,15 @@
 # make script for unit tests
 
 
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o driver.o driver.cpp
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o memory.o memPackage/memory.cpp
-g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o hex.o memPackage/hex.cpp
+#g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o memory.o memPackage/memory.cpp
+#g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o hex.o memPackage/hex.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o hex.o hex.cpp
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o memory.o memory.cpp
+
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o sudokuSolver.o sudokuSolver.cpp
+
+g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -c -o driver.o driver.cpp
+
 g++ -g -ansi -pedantic -Wall -Werror -std=c++14 -o unitTest driver.o sudokuSolver.o memory.o hex.o
 
 
