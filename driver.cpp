48ace6e526820db87aedc6b4d22209d0c6a95ce5
Blue1818 <iwadale@gmail.com>
2022-04-13 11:08:05 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -8,19 +8,22 @@
 int main()
 {
 
+    memory mem(0x1000);
+
+    //memory before file
+    mem.dump();
+    
+    //load puzzle
+    //if fail then show error
+    if (!mem.load_file("samplePuzzles/ARuud.sdk"))
+    {
+        std::cerr << "can't load file";
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
+    //dump file
+    mem.dump();
 
 
 
